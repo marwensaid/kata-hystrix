2269bdc9de979619abb7c2637e826918f41be244
Marwen <marwensaidi1@gmail.com>
2019-12-13 09:31:58 +0100

    solution

--- spring-hystrix-student-service/src/main/java/com/example/howtodoinjava/springhystrixstudentservice/domain/Student.java
+++ spring-hystrix-student-service/src/main/java/com/example/howtodoinjava/springhystrixstudentservice/domain/Student.java
@@ -1,7 +1,29 @@
 package com.example.howtodoinjava.springhystrixstudentservice.domain;
 
 public class Student {
+	private String name;
+	private String className;
 
-    // 1- student must have a name and class
+	
+	public Student(String name, String className) {
+		super();
+		this.name = name;
+		this.className = className;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
 }
