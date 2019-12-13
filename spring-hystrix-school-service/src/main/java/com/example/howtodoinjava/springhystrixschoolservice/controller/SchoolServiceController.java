2ab8018ad0ed128f4ccb4b12e6d14b3cae334ea8
marwen.saidi <marwen.saidi@orange.com>
2019-12-13 10:22:29 +0100

    code

--- spring-hystrix-school-service/src/main/java/com/example/howtodoinjava/springhystrixschoolservice/controller/SchoolServiceController.java
+++ spring-hystrix-school-service/src/main/java/com/example/howtodoinjava/springhystrixschoolservice/controller/SchoolServiceController.java
@@ -1,13 +1,21 @@
 package com.example.howtodoinjava.springhystrixschoolservice.controller;
 
+import com.example.howtodoinjava.springhystrixschoolservice.delegate.StudentServiceDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SchoolServiceController {
 
-	// 1- REST : expose school + call student service to det data
-	
-	public String getStudents(@PathVariable String schoolname) {
-		return "";
-	}
-	
+	@Autowired
+    StudentServiceDelegate studentServiceDelegate;
+
+    // 1- REST : expose school + call student service to det data
+    @GetMapping(value = "/getSchoolDetails/{schoolname}")
+    public String getStudents(@PathVariable String schoolname) {
+        return studentServiceDelegate.callStudentServiceAndGetData(schoolname);
+    }
+
 }
